3c91289e3a4858740bcb92be759e5a2e640418aa
Ganesh Mule <grmule018@gmail.com>
2019-09-08 16:05:04 +0530

    opps

---
+++ GaneshM/src/com/bridgelabz/utility/Node.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.utility;
+
+public class Node <T>{
+//creating node
+	public Object data;
+	public Node<T> next;
+//assign values	
+	public Node()
+	{
+		this.data = null;
+		this.next = null;
+	}
+}
